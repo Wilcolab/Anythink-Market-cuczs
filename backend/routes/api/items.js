9180eb3cc52e47a6401509b189a499f142cd1df8
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-08-07 21:45:51 -0400

    api searching by title

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,7 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
   
    if (typeof req.query.title !== "undefinded") {
-    query.title = { $in: [req.query.title]};
+    query.title = { $regex: [req.query.title], $options: i };
   }
 
   Promise.all([
