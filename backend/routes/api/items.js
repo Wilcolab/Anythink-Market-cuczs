4ec664a2b298216841ba989905cff4883230c0d6
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-08-07 22:17:10 -0400

    added quotes around the options for query

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,7 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
   
    if (typeof req.query.title !== "undefinded") {
-    query.title = { $regex: req.query.title, $options: i };
+    query.title = { $regex: req.query.title, $options: 'i' };
   }
 
   Promise.all([
