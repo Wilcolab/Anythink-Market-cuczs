2e22013ea114b9baaf1614cdb3d5c6a304673626
Brianna <104288486+bbland1@users.noreply.github.com>
2022-08-07 21:28:41 -0400

    add api search filtering

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -52,6 +52,10 @@ router.get("/", auth.optional, function(req, res, next) {
   if (typeof req.query.tag !== "undefined") {
     query.tagList = { $in: [req.query.tag] };
   }
+  
+   if (typeof req.query.title !== "undefinded") {
+    query.title = { $in: [req.query.title]};
+  }
 
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
