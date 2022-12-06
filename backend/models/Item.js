0d94672c1a672c0b7a14007ef9f33cc68d9b3288
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-12-05 21:56:28 -0500

    fixing backend broken image issue to add the placeholder in user method item schema

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -3,6 +3,8 @@ var uniqueValidator = require("mongoose-unique-validator");
 var slug = require("slug");
 var User = mongoose.model("User");
 
+const noImageFallback = "placeholder.png";
+
 var ItemSchema = new mongoose.Schema(
   {
     slug: { type: String, lowercase: true, unique: true },
@@ -19,7 +21,7 @@ var ItemSchema = new mongoose.Schema(
 
 ItemSchema.plugin(uniqueValidator, { message: "is already taken" });
 
-ItemSchema.pre("validate", function(next) {
+ItemSchema.pre("validate", function (next) {
   if (!this.slug) {
     this.slugify();
   }
@@ -27,29 +29,29 @@ ItemSchema.pre("validate", function(next) {
   next();
 });
 
-ItemSchema.methods.slugify = function() {
+ItemSchema.methods.slugify = function () {
   this.slug =
     slug(this.title) +
     "-" +
     ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
 };
 
-ItemSchema.methods.updateFavoriteCount = function() {
+ItemSchema.methods.updateFavoriteCount = function () {
   var item = this;
 
-  return User.count({ favorites: { $in: [item._id] } }).then(function(count) {
+  return User.count({ favorites: { $in: [item._id] } }).then(function (count) {
     item.favoritesCount = count;
 
     return item.save();
   });
 };
 
-ItemSchema.methods.toJSONFor = function(user) {
+ItemSchema.methods.toJSONFor = function (user) {
   return {
     slug: this.slug,
     title: this.title,
     description: this.description,
-    image: this.image,
+    image: this.image || noImageFallback,
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
     tagList: this.tagList,
