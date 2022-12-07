28554ec869b803ed1ef748e2885ea662cd0090a2
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-12-06 20:42:52 -0500

    adding search functiionality to homepage frontend

--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -3,6 +3,7 @@ import {
   ITEM_UNFAVORITED,
   SET_PAGE,
   APPLY_TAG_FILTER,
+  APPLY_TITLE_FILTER,
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
   CHANGE_TAB,
@@ -45,6 +46,17 @@ const reducer = (state = {}, action) => {
         tab: null,
         tag: action.tag,
         currentPage: 0,
+      };
+    case APPLY_TITLE_FILTER:
+      return {
+        ...state,
+        pager: action.pager,
+        items: action.payload.items,
+        itemsCount: action.payload.itemsCount,
+        tab: null,
+        title: action.title,
+        currentPage: 0,
+        noResults: action.payload.items.length === 0,
       };
     case HOME_PAGE_LOADED:
       return {
