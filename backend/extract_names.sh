55a988ab18a604b6039abb36d347417279e02eff
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-12-17 03:44:57 -0500

    adjusting script to only output the names it finds

--- backend/extract_names.sh
+++ backend/extract_names.sh
@@ -11,7 +11,13 @@ fi
 file=$1
 
 # search the file for lines that have the @amazon.com email and save to a file
-grep "@amazon.com" $file > result.txt
+grep "@amazon.com" $file
 
-# print info to the console
-cat result.txt
+# after finding the lines with that email suffix it pulls out only the data in columns 2 and 3
+cut -d"," -f2,3 
+
+# replaces the , inbetween the names with a space
+sed 's/,/ /g'
+
+# print the info in the second column first then the info in the first coulmn to result.txt
+awk '{print $2, $1}' result.txt
