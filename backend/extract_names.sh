2bc5afd6be9cf1cf44805bd232c614fb984299c3
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-12-17 04:09:01 -0500

    fixing grep to search properly

--- backend/extract_names.sh
+++ backend/extract_names.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-cat $1
-
-# search the file for lines that have the @amazon.com email and save to a file
-grep -i "@amazon.com"
-
+# open the file at the first argument
+# search the file for lines that have the @amazon.com email case insensitive
 # after finding the lines with that email suffix it pulls out only the data in columns 2 and 3
-cut -d"," -f2,3 
-
 # replaces the , inbetween the names with a space
-sed 's/,/ /g'
-
 # print the info in the second column first then the info in the first coulmn to arg space 2
-awk '{print $2, $1}' > $2
+cat $1 | grep -i @amazon.com | cut -d"," -f 2-3 | sed 's/,/ /g' | awk '{print $2, $1}' > $2
