117d00f5761893f30eda0bd0ec25353d8d795ec0
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-12-17 04:13:08 -0500

    some spacing changes

--- backend/extract_names.sh
+++ backend/extract_names.sh
@@ -5,4 +5,5 @@
 # after finding the lines with that email suffix it pulls out only the data in columns 2 and 3
 # replaces the , inbetween the names with a space
 # print the info in the second column first then the info in the first coulmn to arg space 2
-cat $1 | grep -i @amazon.com | cut -d"," -f 2-3 | sed 's/,/ /g' | awk '{print $2, $1}' > $2
+
+cat $1 | grep -i @amazon.com | cut -d "," -f 2-3 | sed 's/,/ /g' | awk '{print $2, $1}' > $2
