8a13e5b5a4f1237979fef1add424dd8e9787982d
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-12-17 03:59:54 -0500

    removing arg check

--- backend/extract_names.sh
+++ backend/extract_names.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
 cat $1
-# check if a file was actually passed into the argument spot
-if [ $# -eq 0 ]
-then
-  echo "Error: No file provided"
-  exit 1
-fi
-# assign the first argument passed into a variable
-file=$1
 
 # search the file for lines that have the @amazon.com email and save to a file
 grep -i "@amazon.com" $file
